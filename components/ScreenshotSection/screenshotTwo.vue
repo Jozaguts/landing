94eef2fc259dfc53027dc20640e8a3d2ef844742
Jose Terrazas <jozaguts@gmail.com>
2025-01-05 02:46:46 -0600

    fix(carousel) images

--- components/ScreenshotSection/screenshotTwo.vue
+++ components/ScreenshotSection/screenshotTwo.vue
@@ -1,51 +1,51 @@
 <template>
-    <section id="screenshots" class="section screenshots-area ptb_100   bg-gray overflow-hidden ptb_100">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-12 col-md-10 col-lg-7">
-                        <!-- Section Heading -->
-                        <div class="section-heading text-center">
-                            <h2 class="text-capitalize">Interfaz Simple y Atractiva</h2>
-                            <p class="d-none d-sm-block mt-4">Futzo ofrece una interfaz clara y organizada, diseñada para facilitar la gestión eficiente de tu liga de fútbol. Navega fácilmente por todas las funciones clave.</p>
-                            <p class="d-block d-sm-none mt-4">Futzo te facilita gestionar tu liga con una interfaz intuitiva y visualmente atractiva.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <!-- App Screenshot Slider Area -->
-                        <div class="app-screenshots">
-                            <!-- Single Screenshot Item -->
-                            <div class="single-screenshot">
-                                <img src="assets/img/slider1.png" alt="pagina de registro">
-                            </div>
-                            <!-- Single Screenshot Item -->
-                            <div class="single-screenshot">
-                                <img src="assets/img/slider2.png" alt="">
-                            </div>
-                            <!-- Single Screenshot Item -->
-                            <div class="single-screenshot">
-                                <img src="assets/img/slider3.png" alt="">
-                            </div>
-                            <!-- Single Screenshot Item -->
-                            <div class="single-screenshot">
-                                <img src="assets/img/slider4.png" alt="">
-                            </div>
-                            <!-- Single Screenshot Item -->
-                            <div class="single-screenshot">
-                                <img src="assets/img/slider5.png" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <section id="screenshots" class="section screenshots-area ptb_100   bg-gray overflow-hidden ptb_100">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-7">
+          <!-- Section Heading -->
+          <div class="section-heading text-center">
+            <h2 class="text-capitalize">Interfaz Simple y Atractiva</h2>
+            <p class="d-none d-sm-block mt-4">Futzo ofrece una interfaz clara y organizada, diseñada para facilitar la gestión eficiente de tu liga de fútbol. Navega fácilmente por
+              todas las funciones clave.</p>
+            <p class="d-block d-sm-none mt-4">Futzo te facilita gestionar tu liga con una interfaz intuitiva y visualmente atractiva.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <!-- App Screenshot Slider Area -->
+          <div class="app-screenshots">
+            <!-- Single Screenshot Item -->
+            <div class="single-screenshot">
+              <img src="assets/img/slider1-resized.png" alt="pagina de registro" width="250" height="444">
             </div>
-        </section>
+            <!-- Single Screenshot Item -->
+            <div class="single-screenshot">
+              <img src="assets/img/slider2-resized.png" alt="estadísticas de tu liga" width="250" height="444">
+            </div>
+            <!-- Single Screenshot Item -->
+            <div class="single-screenshot">
+              <img src="assets/img/image4 - laptop - teams-resized.png" alt="Calendario de partidos" width="250" height="501">
+            </div>
+            <div class="single-screenshot">
+              <img src="assets/img/slider2-resized.png" alt="Registros de jugadores" width="250" height="444">
+            </div>
+            <div class="single-screenshot">
+              <img src="assets/img/slider1-resized.png" alt="pagina de registro" width="250" height="444">
+            </div>
+            <div class="single-screenshot">
+              <img src="assets/img/slider2-resized.png" alt="Registros de jugadores" width="250" height="444">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style>
