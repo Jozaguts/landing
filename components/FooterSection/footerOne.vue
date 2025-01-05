98d55ac65cb231179bf8770489bafe4feb647a42
Jose Terrazas <jozaguts@gmail.com>
2025-01-05 01:09:49 -0600

    fix(images) logo footer size

--- components/FooterSection/footerOne.vue
+++ components/FooterSection/footerOne.vue
@@ -1,89 +1,77 @@
 <template>
-    <div>
-        <div class="height-emulator d-none d-lg-block "></div>
-        <footer class="footer-area footer-fixed bg-gray">
-            <!-- Footer Top -->
-            <div class="footer-top ptb_100">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-sm-6 col-lg-3">
-                            <!-- Footer Items -->
-                            <div class="footer-items">
-                                <!-- Logo -->
-                                <a class="navbar-brand" href="#">
-                                    <img class="logo" src="assets/img/logos/vertical/logo-07.png" alt="futzo plataforma admintrativa de ligas deportivas">
-                                </a>
-                                <p class="mt-2 mb-3">El control total de tu liga de fútbol, en un solo lugar.</p>
-                                <!-- Social Icons -->
-                                <div class="social-icons d-flex">
-                                    <a class="facebook" href="https://www.facebook.com/futzo.io">
-                                        <i class="fab fa-facebook-f"></i>
-                                        <i class="fab fa-facebook-f"></i>
-                                    </a>
-                                    <a class="bg-danger" href="https://www.instagram.com/futzo.io/">
-                                        <i class="fab fa-instagram"></i>
-                                        <i class="fab fa-instagram"></i>
-                                    </a>
+  <div>
+    <div class="height-emulator d-none d-lg-block "></div>
+    <footer class="footer-area footer-fixed bg-gray">
+      <!-- Footer Top -->
+      <div class="footer-top ptb_100">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-sm-6 col-lg-3">
+              <!-- Footer Items -->
+              <div class="footer-items">
+                <!-- Logo -->
+                <a class="navbar-brand" href="javascript:void(0)">
+                  <img class="logo" src="assets/img/logos/vertical/logo-07.png" alt="futzo plataforma admintrativa de ligas deportivas" width="240" height="169">
+                </a>
+                <p class="mt-2 mb-3">El control total de tu liga de fútbol, en un solo lugar.</p>
+                <!-- Social Icons -->
+                <div class="social-icons d-flex">
+                  <a class="facebook" href="https://www.facebook.com/futzo.io">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                  <a class="bg-danger" href="https://www.instagram.com/futzo.io/">
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-instagram"></i>
+                  </a>
 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-sm-6 col-lg-3">
-                            <!-- Footer Items -->
-                            <div class="footer-items">
-                                <!-- Footer Title -->
-                                <h3 class="footer-title mb-2">Enlaces útiles</h3>
-                                <ul>
-                                    <li class="py-2"><a href="#home">Home</a></li>
-                                    <li class="py-2"><a href="#features">Características</a></li>
-                                    <li class="py-2"><a href="#screenshots">Imágenes</a></li>
-<!--                                    <li class="py-2"><a href="#contact">Contacto</a></li>-->
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-12 col-sm-6 col-lg-3">
-                            <!-- Footer Items -->
-                            <div class="footer-items">
-                                <!-- Footer Title -->
-                                <h3 class="footer-title mb-2">Ayuda del producto</h3>
-                                <ul>
-                                    <li class="py-2"><a href="#faq">Preguntas frecuentes</a></li>
-                                    <li class="py-2"><a href="#">Política de privacidad</a></li>
-<!--                                    <li class="py-2"><a href="#">Ayuda</a></li>-->
-<!--                                    <li class="py-2"><a href="#">Términos y condiciones</a></li>-->
-<!--                                    <li class="py-2"><a href="#contact">Contacto</a></li>-->
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
                 </div>
+              </div>
             </div>
-            <!-- Footer Bottom -->
-            <div class="footer-bottom">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <!-- Copyright Area -->
-                            <div class="copyright-area d-flex flex-wrap justify-content-center justify-content-sm-between text-center py-4">
-                                <!-- Copyright Left -->
-                                <div class="copyright-left">&copy; Copyrights {{new Date().getFullYear()}} Futzo todos los derechos reservados.</div>
-                                <!-- Copyright Right -->
-<!--                                <div class="copyright-right">Made with <i class="fas fa-heart"></i> By <a href="#">Theme Land</a></div>-->
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-12 col-sm-6 col-lg-3">
+              <!-- Footer Items -->
+              <div class="footer-items">
+                <!-- Footer Title -->
+                <h3 class="footer-title mb-2">Enlaces útiles</h3>
+                <ul>
+                  <li class="py-2"><a href="#home">Home</a></li>
+                  <li class="py-2"><a href="#features">Características</a></li>
+                  <li class="py-2"><a href="#screenshots">Imágenes</a></li>
+                </ul>
+              </div>
             </div>
-        </footer>
-    </div>
+            <div class="col-12 col-sm-6 col-lg-3">
+              <!-- Footer Items -->
+              <div class="footer-items">
+                <!-- Footer Title -->
+                <h3 class="footer-title mb-2">Ayuda del producto</h3>
+                <ul>
+                  <li class="py-2"><a href="#faq">Preguntas frecuentes</a></li>
+                  <li class="py-2"><a href="#">Política de privacidad</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Footer Bottom -->
+      <div class="footer-bottom">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <!-- Copyright Area -->
+              <div class="copyright-area d-flex flex-wrap justify-content-center justify-content-sm-between text-center py-4">
+                <!-- Copyright Left -->
+                <div class="copyright-left">&copy; Copyrights {{ new Date().getFullYear() }} Futzo todos los derechos reservados.</div>
+                <!-- Copyright Right -->
+                <!--                                <div class="copyright-right">Made with <i class="fas fa-heart"></i> By <a href="#">Theme Land</a></div>-->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
-<script>
-export default {
-    
-}
-</script>
-
-<style>
-
-</style>
+<script lang="ts" setup></script>
