e1b1e84554d3ffe7789c2d3a4e176845722b0c61
Jose Terrazas <jozaguts@gmail.com>
2025-01-05 01:03:22 -0600

    fix(images) logo size

--- components/HeaderSection/headerTwo.vue
+++ components/HeaderSection/headerTwo.vue
@@ -1,50 +1,49 @@
 <template>
-    <header class="navbar navbar-sticky navbar-expand-lg navbar-dark">
-            <div class="container position-relative">
-                <a class="navbar-brand" href="/">
-                    <img class="navbar-brand-regular"
-                         width="80"
-                         src="assets/img/logos/icon/logo-02.png" alt="brand-logo"
-                    >
-                    <img class="navbar-brand-sticky"  width="80" src="assets/img/logos/icon/logo-02.png" alt="sticky brand-logo">
-                </a>
-                <button class="navbar-toggler d-lg-none" type="button" data-toggle="navbarToggler" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  <header class="navbar navbar-sticky navbar-expand-lg navbar-dark">
+    <div class="container position-relative">
+      <a class="navbar-brand" href="/">
+        <img class="navbar-brand-regular"
+             width="80"
+             height="56"
+             src="assets/img/logos/icon/logo-02.png" alt="brand-logo"
+        >
+        <img class="navbar-brand-sticky" width="80" src="assets/img/logos/icon/logo-02.png" alt="sticky brand-logo">
+      </a>
+      <button class="navbar-toggler d-lg-none" type="button" data-toggle="navbarToggler" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-                <div class="navbar-inner">
-                    <!--  Mobile Menu Toggler -->
-                    <button class="navbar-toggler d-lg-none" type="button" data-toggle="navbarToggler" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <nav>
-                        <ul class="navbar-nav" id="navbar-nav">
-                          <li class="nav-item">
-                            <a class="nav-link scroll" href="#home">Home</a>
-                          </li>
-                            <li class="nav-item">
-                                <a class="nav-link scroll" href="#features">Características</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link scroll" href="#screenshots">Imágenes</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link scroll" href="#pricing">Precio</a>
-                            </li>
-<!--                            <li class="nav-item">-->
-<!--                                <a class="nav-link scroll" href="#contact">Contacto</a>-->
-<!--                            </li>-->
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
+      <div class="navbar-inner">
+        <!--  Mobile Menu Toggler -->
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="navbarToggler" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <nav>
+          <ul class="navbar-nav" id="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link scroll" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link scroll" href="#features">Características</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link scroll" href="#screenshots">Imágenes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link scroll" href="#pricing">Precio</a>
+            </li>
+            <!--                            <li class="nav-item">-->
+            <!--                                <a class="nav-link scroll" href="#contact">Contacto</a>-->
+            <!--                            </li>-->
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style>
