6717d7eba1e94be51600d6a3d621ecab89eadabe
Jose Terrazas <jozaguts@gmail.com>
2025-01-05 01:18:30 -0600

    fix(images) discover image

--- components/DiscoverSection/discoverTwo.vue
+++ components/DiscoverSection/discoverTwo.vue
@@ -1,72 +1,65 @@
 <template>
-    <section class="section discover-area bg-gray overflow-hidden ptb_100">
-            <div class="container">
-                <div class="row justify-content-between align-items-center">
-                    <div class="col-12 col-lg-6 order-2 order-lg-1">
-                        <!-- Discover Thumb -->
-                        <div class="service-thumb discover-thumb mx-auto text-center">
-                            <img src="assets/img/image 3 - floating - stats.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-12 col-lg-6 order-1 order-lg-2">
-                        <!-- Discover Text -->
-                        <div class="discover-text px-0 px-lg-4 pt-4 pt-lg-0">
-                            <h2 class="pb-4">Explora nuestras funciones avanzadas.</h2>
-                            <!-- Check List -->
-                            <ul class="check-list">
-                                <li class="py-1">
-                                    <!-- List Box -->
-                                    <div class="list-box media">
-                                        <span class="icon align-self-center"><i class="fas fa-check"></i></span>
-                                        <span class="media-body pl-2">Registro y administración completa de equipos, jugadores y cuerpo técnico, con información detallada y precisa. Además, genera URLs únicas para que equipos y jugadores se registren automáticamente en los torneos.</span>
-                                    </div>
-                                </li>
-                                <li class="py-1">
-                                    <!-- List Box -->
-                                    <div class="list-box media">
-                                        <span class="icon align-self-center"><i class="fas fa-check"></i></span>
-                                        <span class="media-body pl-2">Automatización de calendarios de partidos: Creación automática de calendarios considerando disponibilidad de estadios y preferencias de equipos.</span>
-                                    </div>
-                                </li>
-                                <li class="py-1">
-                                    <!-- List Box -->
-                                    <div class="list-box media">
-                                        <span class="icon align-self-center"><i class="fas fa-check"></i></span>
-                                        <span class="media-body pl-2">Notificaciones en tiempo real: Notificaciones automáticas sobre partidos próximos, cambios en el calendario y actualizaciones relevantes.</span>
-                                    </div>
-                                </li>
-                                <li class="py-1">
-                                    <!-- List Box -->
-                                    <div class="list-box media">
-                                        <span class="icon align-self-center"><i class="fas fa-check"></i></span>
-                                        <span class="media-body pl-2">Estadísticas detalladas y análisis: Recopilación y presentación de estadísticas de juegos y rendimiento de equipos y jugadores</span>
-                                    </div>
-                                </li>
-                            </ul>
-                            <div class="icon-box d-flex mt-3 mb-3">
-                                <div class="service-icon">
-                                    <span><i class="fas fa-bell"></i></span>
-                                </div>
-                                <div class="service-icon px-3">
-                                    <span><i class="fas fa-calendar-alt"></i></span>
-                                </div>
-                                <div class="service-icon">
-                                    <span><i class="fas fa-users-cog"></i></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <section class="section discover-area bg-gray overflow-hidden ptb_100">
+    <div class="container">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-12 col-lg-6 order-2 order-lg-1">
+          <!-- Discover Thumb -->
+          <div class="service-thumb discover-thumb mx-auto text-center">
+            <img src="assets/img/image 3 - floating - stats.png" alt="explora nuestras funciones" width="540" height="359">
+          </div>
+        </div>
+        <div class="col-12 col-lg-6 order-1 order-lg-2">
+          <!-- Discover Text -->
+          <div class="discover-text px-0 px-lg-4 pt-4 pt-lg-0">
+            <h2 class="pb-4">Explora nuestras funciones avanzadas.</h2>
+            <!-- Check List -->
+            <ul class="check-list">
+              <li class="py-1">
+                <!-- List Box -->
+                <div class="list-box media">
+                  <span class="icon align-self-center"><i class="fas fa-check"></i></span>
+                  <span class="media-body pl-2">Registro y administración completa de equipos, jugadores y cuerpo técnico, con información detallada y precisa. Además, genera URLs únicas para que equipos y jugadores se registren automáticamente en los torneos.</span>
                 </div>
+              </li>
+              <li class="py-1">
+                <!-- List Box -->
+                <div class="list-box media">
+                  <span class="icon align-self-center"><i class="fas fa-check"></i></span>
+                  <span class="media-body pl-2">Automatización de calendarios de partidos: Creación automática de calendarios considerando disponibilidad de estadios y preferencias de equipos.</span>
+                </div>
+              </li>
+              <li class="py-1">
+                <!-- List Box -->
+                <div class="list-box media">
+                  <span class="icon align-self-center"><i class="fas fa-check"></i></span>
+                  <span class="media-body pl-2">Notificaciones en tiempo real: Notificaciones automáticas sobre partidos próximos, cambios en el calendario y actualizaciones relevantes.</span>
+                </div>
+              </li>
+              <li class="py-1">
+                <!-- List Box -->
+                <div class="list-box media">
+                  <span class="icon align-self-center"><i class="fas fa-check"></i></span>
+                  <span
+                      class="media-body pl-2">Estadísticas detalladas y análisis: Recopilación y presentación de estadísticas de juegos y rendimiento de equipos y jugadores</span>
+                </div>
+              </li>
+            </ul>
+            <div class="icon-box d-flex mt-3 mb-3">
+              <div class="service-icon">
+                <span><i class="fas fa-bell"></i></span>
+              </div>
+              <div class="service-icon px-3">
+                <span><i class="fas fa-calendar-alt"></i></span>
+              </div>
+              <div class="service-icon">
+                <span><i class="fas fa-users-cog"></i></span>
+              </div>
             </div>
-        </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script>
-export default {
-    
-}
-</script>
-
-<style>
-
-</style>
+<script lang="ts" setup></script>
